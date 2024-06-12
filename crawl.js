d0ce2ce326276ae7ea493849e5fdd3c0a6446a27
DomenicoDicosimo <domenicdicosimo@gmail.com>
2024-06-12 13:08:50 -0400

    added crawlpage

--- crawl.js
+++ crawl.js
@@ -22,6 +22,28 @@ function getURLsFromHtml(htmlBody, baseURL){
         }
     }
     return urls
-}   
+}
 
-export { normalizeURL, getURLsFromHtml };
+async function crawlPage(currentURL){
+    try {
+        const response = await fetch(currentURL);
+        
+        if (response.status >= 400){
+            console.error(`Error: Recieved status code ${response.status}`)
+            return
+        }
+
+        const contentType = response.headers.get("content-type");
+        if (!contentType || !contentType.includes("text/html")){
+            console.error(`Error: Expected content-type text/html buy received ${contentType}`)
+            return
+        }
+
+        const html = await response.text();
+        console.log(html);
+    }catch (error) {
+        console.error(`Fetch error: ${error.message}`);
+    }
+}
+
+export { normalizeURL, getURLsFromHtml, crawlPage };
--- main.js
+++ main.js
@@ -1,1 +1,22 @@
-console.log('hello world')
+import { argv } from 'node:process';
+import { crawlPage } from './crawl.js'; 
+
+function main() {
+    try {
+        if (argv.length < 3){
+            throw new Error('No URL was passed in.')
+        }
+        else if (argv.length > 3){
+            throw new Error(`Too many URLs were passed in. ${argv}`)
+        }
+        else{
+            console.log(`The crawler is starting at ${argv[2]}`)
+            crawlPage(argv[2])
+        }
+
+    } catch (err) {
+        console.log(err.message)
+    }
+}
+
+main()
